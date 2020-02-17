5d4b0441cec452f0fddefe2a692ee4c855be4a51
yqrashawn <namy.19@gmail.com>
2020-02-17 17:26:55 +0800

    Add: test changelog feature

--- development/auto-changelog.sh
+++ development/auto-changelog.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -o pipefail
 
-# readonly URL='https://github.com/Conflux-Chain/conflux-portal'
+readonly URL='https://github.com/Conflux-Chain/conflux-portal'
 
 git fetch --tags
 
@@ -12,15 +12,13 @@ most_recent_tag="$(git describe --tags "$(git rev-list --tags --max-count=1)")"
 
 git rev-list "${most_recent_tag}"..HEAD | while read -r commit
 do
-    # commit message
     subject="$(git show -s --format="%s" "$commit")"
 
     # Squash & Merge: the commit subject is parsed as `<description> (#<PR ID>)`
     if grep -E -q '\(#[[:digit:]]+\)' <<< "$subject"
     then
-        # pr="$(awk '{print $NF}' <<< "$subject" | tr -d '()')"
-        # prefix="[$pr]($URL/pull/${pr###}): "
-        prefix=''
+        pr="$(awk '{print $NF}' <<< "$subject" | tr -d '()')"
+        prefix="[$pr]($URL/pull/${pr###}): "
         description="$(awk '{NF--; print $0}' <<< "$subject")"
 
     # Merge: the PR ID is parsed from the git subject (which is of the form `Merge pull request
@@ -28,9 +26,8 @@ do
     #   If no body is found, the description is set to the commit subject
     elif grep -E -q '#[[:digit:]]+\sfrom' <<< "$subject"
     then
-        # pr="$(awk '{print $4}' <<< "$subject")"
-        # prefix="[$pr]($URL/pull/${pr###}): "
-        prefix=''
+        pr="$(awk '{print $4}' <<< "$subject")"
+        prefix="[$pr]($URL/pull/${pr###}): "
 
         first_line_of_body="$(git show -s --format="%b" "$commit" | head -n 1 | tr -d '\r')"
         if [[ -z "$first_line_of_body" ]]
@@ -42,7 +39,7 @@ do
 
     # Normal commits: The commit subject is the description, and the PR ID is omitted.
     else
-        # pr=''
+        pr=''
         prefix=''
         description="$subject"
     fi
