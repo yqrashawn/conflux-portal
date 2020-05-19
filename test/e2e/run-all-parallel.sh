d79a575f8f48735543a14d209e007c91363c7aba
yqrashawn <namy.19@gmail.com>
2020-05-19 16:21:15 +0800

    Fix: e2e tests

--- test/e2e/run-all-parallel.sh
+++ test/e2e/run-all-parallel.sh
@@ -1,55 +1,53 @@
 #!/usr/bin/env bash
 
 set -x
-set -e
-set -u
 set -o pipefail
 
 export PATH="$PATH:./node_modules/.bin"
 
-[ "$BUILDKITE_PARALLEL_JOB" = 0 ] && mocha --no-timeouts test/e2e/tests/*.spec.js
+[ "$BUILDKITE_PARALLEL_JOB" = '0' ] && mocha --no-timeouts test/e2e/tests/*.spec.js
 
-[ "$BUILDKITE_PARALLEL_JOB" = 1 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '1' ] && concurrently --kill-others \
   --names 'dapp,e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
   'yarn dapp' \
   'mocha test/e2e/metamask-ui.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 2 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '2' ] && concurrently --kill-others \
   --names 'dapp,e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
   'yarn dapp' \
   'mocha test/e2e/metamask-responsive-ui.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 3 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '3' ] && concurrently --kill-others \
   --names 'dapp,e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
   'yarn dapp' \
   'mocha test/e2e/signature-request.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 4 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '4' ] && concurrently --kill-others \
   --names 'e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
   'mocha test/e2e/from-import-ui.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 5 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '5' ] && concurrently --kill-others \
   --names 'e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
   'mocha test/e2e/send-edit.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 6 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '6' ] && concurrently --kill-others \
   --names 'dapp,e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
   'yarn dapp' \
   'mocha test/e2e/ethereum-on.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 7 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '7' ] && concurrently --kill-others \
   --names 'dapp,e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
@@ -63,7 +61,7 @@ export PATH="$PATH:./node_modules/.bin"
 #   'yarn sendwithprivatedapp' \
 #   'mocha test/e2e/incremental-security.spec'
 
-[ "$BUILDKITE_PARALLEL_JOB" = 8 ] && concurrently --kill-others \
+[ "$BUILDKITE_PARALLEL_JOB" = '8' ] && concurrently --kill-others \
   --names 'dapp,e2e' \
   --prefix '[{time}][{name}]' \
   --success first \
