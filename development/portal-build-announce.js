3cdab843621f8ed2bdd2e275545e6b4180eabe4f
yqrashawn <namy.19@gmail.com>
2020-05-22 19:15:11 +0800

    Add: WIP gh comment

---
+++ development/portal-build-announce.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// const { Octokit } = require('@octokit/rest')
+
+// const octokit = new Octokit({
+//   auth: process.env.GH_TOKEN,
+// })
+
+// // Create a comment under an issue
+// octokit.issues.createComment({
+//   owner: 'yqrashawn',
+//   repo: 'conflux-portal',
+//   issue_number,
+//   body,
+// })
+
+// Create a content attachment
